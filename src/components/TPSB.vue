cf2c2be31202671211f06a970ce42a4d0c2408af
Danielle Lapensee-Rankine <lapenseerankinedl@appstate.edu>
2021-12-02 17:32:26 -0500

    Added 41 E2E tests

--- src/components/TPSB.vue
+++ src/components/TPSB.vue
@@ -1,55 +1,146 @@
 <template>
   <section>
     <div class="tpsb">
-      <b-row><router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/rqg5-mkef.json', hdr: msg} }"><h1>{{ msg }}</h1></router-link></b-row>
+      <b-row
+        ><router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/rqg5-mkef.json',
+              hdr: msg,
+            },
+          }"
+          ><h1 data-test-id="TPSB-Main-link">{{ msg }}</h1></router-link
+        ></b-row
+      >
       <b-row><h2>Regions</h2></b-row>
-      <b-row>
-        <router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/6tmq-h6uy.json', hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 1 - Boston'} }">
+      <b-row data-test-id="TPSB-Boston-link">
+        <router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/6tmq-h6uy.json',
+              hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 1 - Boston',
+            },
+          }"
+        >
           % Wearing Seat Belts - Region 1 - Boston
         </router-link>
       </b-row>
-      <b-row>
-        <router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/hget-9fst.json', hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 2 - New York'} }">
+      <b-row data-test-id="TPSB-New-York-link">
+        <router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/hget-9fst.json',
+              hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 2 - New York',
+            },
+          }"
+        >
           % Wearing Seat Belts - Region 2 - New York
         </router-link>
       </b-row>
-      <b-row>
-        <router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/hauf-e9a4.json', hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 3 - Philadelphia'} }">
+      <b-row data-test-id="TPSB-Philadelphia-link">
+        <router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/hauf-e9a4.json',
+              hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 3 - Philadelphia',
+            },
+          }"
+        >
           % Wearing Seat Belts - Region 3 - Philadelphia
         </router-link>
       </b-row>
-      <b-row>
-        <router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/vqff-ff7g.json', hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 4 - Atlanta'} }">
+      <b-row data-test-id="TPSB-Atlanta-link">
+        <router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/vqff-ff7g.json',
+              hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 4 - Atlanta',
+            },
+          }"
+        >
           % Wearing Seat Belts - Region 4 - Atlanta
         </router-link>
       </b-row>
-      <b-row>
-        <router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/26a7-nc4u.json', hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 5 - Chicago'} }">
+      <b-row data-test-id="TPSB-Chicago-link">
+        <router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/26a7-nc4u.json',
+              hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 5 - Chicago',
+            },
+          }"
+        >
           % Wearing Seat Belts - Region 5 - Chicago
         </router-link>
       </b-row>
-      <b-row>
-        <router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/4r49-eadb.json', hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 6 - Dallas'} }">
+      <b-row data-test-id="TPSB-Dallas-link">
+        <router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/4r49-eadb.json',
+              hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 6 - Dallas',
+            },
+          }"
+        >
           % Wearing Seat Belts - Region 6 - Dallas
         </router-link>
       </b-row>
-      <b-row>
-        <router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/y8pa-p62q.json', hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 7 - Kansas City'} }">
+      <b-row data-test-id="TPSB-Kansas-link">
+        <router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/y8pa-p62q.json',
+              hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 7 - Kansas City',
+            },
+          }"
+        >
           % Wearing Seat Belts - Region 7 - Kansas City
         </router-link>
       </b-row>
-      <b-row>
-        <router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/iz46-hpaa.json', hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 8 - Denver'} }">
+      <b-row data-test-id="TPSB-Denver-link">
+        <router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/iz46-hpaa.json',
+              hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 8 - Denver',
+            },
+          }"
+        >
           % Wearing Seat Belts - Region 8 - Denver
         </router-link>
       </b-row>
-      <b-row>
-        <router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/fkfk-2j5x.json', hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 9 - San Francisco'} }">
+      <b-row data-test-id="TPSB-San-Francisco-link">
+        <router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/fkfk-2j5x.json',
+              hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 9 - San Francisco',
+            },
+          }"
+        >
           % Wearing Seat Belts - Region 9 - San Francisco
         </router-link>
       </b-row>
-      <b-row>
-        <router-link :to="{name: 'Results', params: {url: 'https://data.cdc.gov/resource/msjj-a7q2.json', hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 10 - Seattle'} }">
+      <b-row data-test-id="TPSB-Seattle-link">
+        <router-link
+          :to="{
+            name: 'Results',
+            params: {
+              url: 'https://data.cdc.gov/resource/msjj-a7q2.json',
+              hdr: 'Percentage of Drivers and Front Seat Passengers Wearing Seat Belts - Region 10 - Seattle',
+            },
+          }"
+        >
           % Wearing Seat Belts - Region 10 - Seattle
         </router-link>
       </b-row>
--- src/components/results.vue
+++ src/components/results.vue
@@ -1,66 +1,68 @@
 <template>
-    <div>
-        <Header msg="Motor Vehicle Statistics" />
-        <b-container>
-            <h2>{{ hdr }}</h2>
-            <b-row>
-                <b-input-group>
-                    <b-col cols="11">
-                        <b-input v-model="filter" placeholder="Search" ></b-input>
-                    </b-col>
-                    <b-col cols="1">
-                        <b-input-group-append>
-                            <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
-                        </b-input-group-append>
-                    </b-col>
-                </b-input-group>
-            </b-row>
-            <b-table striped hover :items="items" :filter="filter"></b-table>
-        </b-container>
-        <Footer msg="Data retrieved from the CDC" />
-    </div>
+  <div>
+    <Header msg="Motor Vehicle Statistics" />
+    <b-container>
+      <h2>{{ hdr }}</h2>
+      <b-row>
+        <b-input-group>
+          <b-col cols="11">
+            <b-input v-model="filter" placeholder="Search" data-test-id="Search-Bar"></b-input>
+          </b-col>
+          <b-col cols="1">
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''"
+                data-test-id="Clear-Button">Clear</b-button
+              >
+            </b-input-group-append>
+          </b-col>
+        </b-input-group>
+      </b-row>
+      <b-table striped hover :items="items" :filter="filter" data-test-id="Results-Table"></b-table>
+    </b-container>
+    <Footer msg="Data retrieved from the CDC" />
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
-    name: 'Results',
-    props: {
-        url: String,
-        hdr: String
-    },
-    components: {
-        Header,
-        Footer
-    },
-    data() {
-        return {
-            filter: '',
-            items: []
-        }
-    },
-    created() {
-        axios.get(this.url)
-        .then(res => {
-            this.items = res.data;
+  name: "Results",
+  props: {
+    url: String,
+    hdr: String,
+  },
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      filter: "",
+      items: [],
+    };
+  },
+  created() {
+    axios
+      .get(this.url)
+      .then((res) => {
+        this.items = res.data;
 
-            for (var i = 0; i < this.items.length; i++) {
-                Object.keys(this.items[i]).some(j => { 
-                    if(j.endsWith('2014')){
-                        this.$delete(this.items[i], j);
-                    }
-                });
+        for (var i = 0; i < this.items.length; i++) {
+          Object.keys(this.items[i]).some((j) => {
+            if (j.endsWith("2014")) {
+              this.$delete(this.items[i], j);
             }
-        })
-        .catch(error => {
-            console.error(error);
-        })
-    }
-}
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
 </script>
 
-<style scoped="scss">
-</style>
+<style scoped="scss"></style>
